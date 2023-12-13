7ef2c42e1c43728c20c7ef8be2ba0c9d830caf08
rishit-singh <rsrishitsingh@gmail.com>
2023-12-12 17:20:24 -0800

    Fixed GMP linking

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -1,21 +1,50 @@
-# Try to find the GNU Multiple Precision Arithmetic Library (GMP)
-# See http://gmplib.org/
-
-if (GMP_INCLUDES AND GMP_LIBRARIES)
-  set(GMP_FIND_QUIETLY TRUE)
-endif (GMP_INCLUDES AND GMP_LIBRARIES)
-
-find_path(GMP_INCLUDES
-  NAMES
-  gmp.h
-  PATHS
-  $ENV{GMPDIR}
-  ${INCLUDE_INSTALL_DIR}
-)
-
-find_library(GMP_LIBRARIES gmp PATHS $ENV{GMPDIR} ${LIB_INSTALL_DIR})
+# Tries to find an install of the GNU multiple precision library
+#
+# Once done this will define
+#   GMP_FOUND - BOOL: System has the GMP library installed
+#   GMP_INCLUDE_DIRS - LIST:The GMP include directories
+#   GMP_C_LIBRARIES - LIST:The libraries needed to use GMP via it's C interface
+#   GMP_CXX_LIBRARIES - LIST:The libraries needed to use GMP via it's C++ interface
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GMP DEFAULT_MSG
-                                  GMP_INCLUDES GMP_LIBRARIES)
-mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
+
+# Try to find libraries
+find_library(GMP_C_LIBRARIES
+    NAMES gmp
+    DOC "GMP C libraries"
+)
+find_library(GMP_CXX_LIBRARIES
+    NAMES gmpxx
+    DOC "GMP C++ libraries"
+)
+
+# Try to find headers
+find_path(GMP_C_INCLUDES
+    NAMES gmp.h
+    DOC "GMP C header"
+)
+
+find_path(GMP_CXX_INCLUDES
+    NAMES gmpxx.h
+    DOC "GMP C++ header"
+)
+
+# TODO: We should check we can link some simple code against libgmp and libgmpxx
+
+# Handle QUIET and REQUIRED and check the necessary variables were set and if so
+# set ``GMP_FOUND``
+find_package_handle_standard_args(GMP
+	REQUIRED_VARS GMP_C_LIBRARIES GMP_C_INCLUDES GMP_CXX_LIBRARIES GMP_CXX_INCLUDES)
+
+if (GMP_FOUND)
+    set(GMP_INCLUDE_DIRS "${GMP_C_INCLUDES}" "${GMP_CXX_INCLUDES}")
+    list(REMOVE_DUPLICATES GMP_INCLUDE_DIRS)
+
+    if (NOT TARGET GMP::GMP)
+    add_library(GMP::GMP UNKNOWN IMPORTED)
+    set_target_properties(GMP::GMP PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${GMP_C_INCLUDES}"
+        IMPORTED_LOCATION "${GMP_C_LIBRARIES}"
+    )
+    endif()
+endif()
