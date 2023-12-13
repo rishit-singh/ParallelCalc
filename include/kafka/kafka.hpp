8351ec09d62367702b7dd5ef7cd6201a28e6fed7
rishit-singh <rsrishitsingh@gmail.com>
2023-12-13 04:08:39 -0800

    Added KafkaConsumers

--- include/kafka/kafka.hpp
+++ include/kafka/kafka.hpp
@@ -7,6 +7,9 @@
 #include <string_view>
 #include <stdexcept>
 #include <fstream>
+#include <iostream> // TODO: REMOVE THIS ASAP
+#include <csignal>
+#include <functional>
 
 using json = nlohmann::json;
 
@@ -14,6 +17,10 @@ using cppkafka::Producer;
 using cppkafka::Configuration;
 using cppkafka::Topic;
 using cppkafka::MessageBuilder;
+using cppkafka::Consumer;
+using cppkafka::Message;
+
+constexpr size_t DefaultPollDelay = 1000;
 
 /// @brief Config for Kafka services
 struct KafkaConfig
@@ -27,12 +34,18 @@ struct KafkaConfig
     /// @brief Topic partition
     size_t Partition;
 
+    /// @brief Groupd Id for the consumer
+    std::string GroupId;
+
+    /// @brief Delay in ms for each call of each consumer poll
+    size_t PollDelay;
+
     /// @brief Loads the config from a file 
     /// @param File path 
     /// @return Loaded config
     static KafkaConfig FromFile(const std::string_view);
 
-    KafkaConfig(std::string_view = nullptr, std::string_view = nullptr, size_t = 0);
+    KafkaConfig(std::string = nullptr, std::string = nullptr, const size_t = 0, std::string = nullptr, const size_t = DefaultPollDelay);
 };
 
 /// @brief Producer abstraction
@@ -54,5 +67,32 @@ public:
     void Stream(std::vector<std::string>);
 
     KafkaProducer(KafkaConfig);
+};
+
+/// @brief Consumer abstraction
+class KafkaConsumer
+{
+private:
+    /// @brief Configuration 
+    KafkaConfig Config;
+    
+    /// @brief Consumer instance
+    Consumer mConsumer;
+
+    /// @brief Callback called when a message is received after polling
+    std::function<void(const Message&)> OnReceiveCallback; 
+
+    /// @brief Running state
+    bool IsRunning;
+
+public: 
+    /// @brief Runs the consumer loop
+    void Run();
+    
+    /// @brief Sets OnReceiveCallback to the given value 
+    /// @param callback Callback to set  
+    void SetOnReceiveCallback(std::function<void(const Message&)>);
+
+    KafkaConsumer(KafkaConfig);
 };
 
--- src/kafka/kafka.cpp
+++ src/kafka/kafka.cpp
@@ -1,7 +1,7 @@
 #include "kafka.hpp"
 
-KafkaConfig::KafkaConfig(std::string_view broker, std::string_view topic, size_t partition)
-    : Broker(broker), Topic(topic), Partition(partition)
+KafkaConfig::KafkaConfig(std::string broker, std::string topic, const size_t partition, std::string groupId, const size_t pollDelay)
+    : Broker(broker), Topic(topic), Partition(partition), GroupId(groupId), PollDelay(pollDelay)
 {
 }
 
@@ -9,10 +9,12 @@ KafkaConfig KafkaConfig::FromFile(const std::string_view file)
 {
     std::ifstream fp(file.data());
 
-    std::array<std::string_view, 3> requiredKeys = {
+    std::array<std::string_view, 5> requiredKeys = {
         "broker",
         "topic",
-        "partition"
+        "partition",
+        "group_id",
+        "poll_delay"
     }; 
 
     json data = json::parse(fp);
@@ -24,7 +26,11 @@ KafkaConfig KafkaConfig::FromFile(const std::string_view file)
             throw std::runtime_error(error + " is required but not provided in the config.");
         }
 
-    return KafkaConfig(data["broker"].template get<std::string_view>(), data["topic"].template get<std::string_view>(), static_cast<size_t>(data["partition"])); 
+    return KafkaConfig(data["broker"].template get<std::string>(), 
+                        data["topic"].template get<std::string>(),
+                        data["partition"].template get<size_t>(),
+                        data["group_id"].template get<std::string>(),
+                        data["poll_delay"].template get<size_t>()); 
 }
 
     
@@ -44,4 +50,52 @@ void KafkaProducer::Stream(std::vector<std::string> messages)
     }
 
     this->mProducer.flush();
+}
+
+
+KafkaConsumer::KafkaConsumer(KafkaConfig config) 
+    : Config(config), mConsumer({
+                { "metadata.broker.list", config.Broker },
+                { "group.id", config.GroupId },
+                { "enable.auto.commit", false }
+        }), IsRunning(false)
+{
+    // signal(SIGINT, [this](int) { this->IsRunning = false; });
+
+    	// Print the assigned partitions on assignment
+	this->mConsumer.set_assignment_callback(
+		[](const cppkafka::TopicPartitionList& partitions) {
+			std::stringstream ss("Got assigned: ");
+			ss << partitions;
+
+
+            std::cout << ss.str();
+			// logger->info(ss.str());
+		});
+
+    this->mConsumer.set_revocation_callback([](const cppkafka::TopicPartitionList& partitions) {
+
+			std::stringstream ss("Got revoked: ");
+			ss << partitions;
+
+            std::cout << ss.str();
+		});
+}
+
+
+void KafkaConsumer::Run()
+{
+    this->mConsumer.subscribe({this->Config.Topic});
+
+    while (this->IsRunning)
+    {
+        Message message = this->mConsumer.poll(std::chrono::milliseconds{this->Config.PollDelay});
+
+        this->OnReceiveCallback(message);
+    }
+}
+
+void KafkaConsumer::SetOnReceiveCallback(std::function<void(const Message&)> callback)
+{
+    this->OnReceiveCallback = callback;
 }
